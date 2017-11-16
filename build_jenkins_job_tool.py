bddf4e3404a1d733e917ac6c48fad53855a663a7
jerrywu0803 <33708953+jerrywu0803@users.noreply.github.com>
2017-11-16 14:00:03 +0800

    Add except and change record file to csv

--- build_jenkins_job_tool.py
+++ build_jenkins_job_tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
-
+import pandas
+from openpyxl import load_workbook
 import string
 import sys
 import xlwt
@@ -19,17 +20,18 @@ def run():
         pn=[]
         with open ('data.txt','r') as f:
             for line in f:
+                #print(line)
                 line=line.strip('\n')
                 pn.append(list(map(str,line.split(','))))
-                print (len(pn))
+                #print (len(pn))
             for i in range (len(pn)):
                 project=str(pn[0][i])
                 tag=str(pn[1][i])
                 buildType=str(pn[2][i])
                 multi(project, tag, buildType)
-                print (pn[0][i])
-                print (pn[1][i])
-                print (pn[2][i])
+                #print (pn[0][i])
+                #print (pn[1][i])
+                #print (pn[2][i])
     else:
         tag = input("請輸入tag : (e.g.1.0.2)" )
         buildType = input("請輸入buildType : (e.g.直接build遊戲)" )
@@ -39,27 +41,36 @@ def thread(project, tag, buildType):
     print("*------專案 : " + project + "------*")
     print("*------tag : " + tag + "------------------------*")
     print("*------buildType : " + buildType +"----------*" )
-    '''JENKINS_URL = "http://192.168.4.125:8080"
-    JENKINS_USER = "qa"
-    JENKINS_PASSWORD = "qa"
-    token = "fc6b3df05ca3f99fc6609ad7ec171133"
-    job = project
-    crumb=CrumbRequester(username=JENKINS_USER, password=JENKINS_PASSWORD, baseurl=JENKINS_URL)
-    j = Jenkins(JENKINS_URL, username=JENKINS_USER, password=JENKINS_PASSWORD, requester=crumb)
-    params = {'tag':tag ,'buildType':buildType}
-    j.build_job(job,params)'''
-    print("*------寫入紀錄黨-------------------------*")
-    book = xlwt.Workbook(encoding = "utf-8", style_compression = 0)
-    sheet = book.add_sheet("sheet1", cell_overwrite_ok = True)
-    sheet.write(0, 0, "project")
-    sheet.write(1, 0, project)
-    sheet.write(0, 1, "版本")
-    sheet.write(1, 1, tag)
-    sheet.write(0, 2, "最後更新時間")
-    sheet.write(1, 2, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    book.save("record.xls")
-    print("*------Done-------------------------------*")
+    try:
+        JENKINS_URL = "http://192.168.4.125:8080"
+        JENKINS_USER = "qa"
+        JENKINS_PASSWORD = "qa"
+        token = "fc6b3df05ca3f99fc6609ad7ec171133"
+        job = project
+        crumb=CrumbRequester(username=JENKINS_USER, password=JENKINS_PASSWORD, baseurl=JENKINS_URL)
+        j = Jenkins(JENKINS_URL, username=JENKINS_USER, password=JENKINS_PASSWORD, requester=crumb)
+        params = {'tag':tag ,'buildType':buildType}
+        j.build_job(job,params)
 
+    except Exception as e:
+        print("*------寫入紀錄檔-------------------------*")
+        f = open("record.csv","a")
+        f.write(project+",")
+        f.write(tag+",")
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+",")
+        f.write("Build fail\n")
+        f.close
+        print("*------Build fail please check the data is correct.------*")
+        pass
+
+    except:
+        print("*------寫入紀錄檔-------------------------*")
+        f = open("record.csv","a")
+        f.write(project+",")
+        f.write(tag+",")
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"\n")
+        f.close
+        print("*------Done-------------------------------*")
 
 def multi(project, tag, buildType):
     jobs = []
